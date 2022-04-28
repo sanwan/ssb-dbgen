875cd9527d0748fe0cd8c67849ea00f7056ad932
sanwan <838244382@qq.com>
2022-04-28 15:05:09 +0800

    Update bm_utils.c

--- bm_utils.c
+++ bm_utils.c
@@ -384,7 +384,7 @@ tbl_open(int tbl, char *mode)
     if (S_ISFIFO(fstats.st_mode))
         {
         retcode =
-            open(fullpath, ((*mode == 'r')?O_RDONLY:O_WRONLY)|O_CREAT);
+            open(fullpath, ((*mode == 'r')?O_RDONLY:O_WRONLY)|O_CREAT,0644);
         f = fdopen(retcode, mode);
         }
     else{
